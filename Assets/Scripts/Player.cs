cdd2221efbb5de9add264ed6d31495a77ae47923
planepaper <eddysonkr@gmail.com>
2022-03-16 22:19:56 +0900

    FEAT: Implement Grounded Feature For One Jump
    - Double Jump can't be allowed in this game.
    - Make him not bump on the platform with not colliding.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,18 +3,48 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
+    private Collider2D _collider;
+    private Collider2D[] _results;
     private Vector2 _direction;
     public float moveSpeed = 1f;
     public float jumpStrength = 1f;
+    private bool grounded;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
+        _results = new Collider2D[4];
+    }
+
+    private async void CheckCollision()
+    {
+        grounded = false;
+
+        Vector2 size = _collider.bounds.size;
+        size.y += 0.1f;
+        size.x /= 2f;
+
+        int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0f, _results);
+
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject hit = _results[i].gameObject;
+
+            if (hit.layer == LayerMask.NameToLayer("Ground"))
+            {
+                grounded = hit.transform.position.y < (transform.position.y - 0.5f);
+
+                Physics2D.IgnoreCollision(_collider, _results[i], !grounded);
+            }
+        }
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        CheckCollision();
+
+        if (grounded && Input.GetButtonDown("Jump"))
         {
             _direction = Vector2.up * jumpStrength;
         }
@@ -24,7 +54,11 @@ public class Player : MonoBehaviour
         }
 
         _direction.x = Input.GetAxis("Horizontal") * moveSpeed;
-        _direction.y = Mathf.Max(_direction.y, -1f);
+
+        if (grounded)
+        {
+            _direction.y = Mathf.Max(_direction.y, -1f);
+        }
 
         if (_direction.x > 0f)
         {
