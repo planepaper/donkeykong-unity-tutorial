852a6ea798455b8c8759037e73fac2b03c2e260d
planepaper <eddysonkr@gmail.com>
2022-03-23 23:27:39 +0900

    FEAT: Implement Level2
    - Make Level2 GameScene.
    - Make PreLoad GameScene for continuing Level2.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,13 +1,37 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    private int level;
     private int lives;
     private int score;
 
     private void Start()
     {
+        DontDestroyOnLoad(gameObject);
         NewGame();
+
+        LoadLevel(1);
+    }
+
+    private void LoadLevel(int index)
+    {
+        level = index;
+
+        Camera camera = Camera.main;
+
+        if (camera != null)
+        {
+            camera.cullingMask = 0;
+        }
+
+        Invoke(nameof(LoadScene), 1f);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(level);
     }
 
     private void NewGame()
@@ -19,6 +43,19 @@ public class GameController : MonoBehaviour
     public void LevelComplete()
     {
         score += 100;
+
+        int nextLevel = level + 1;
+
+        if (nextLevel < SceneManager.sceneCountInBuildSettings)
+        {
+            LoadLevel(nextLevel);
+        }
+        else
+        {
+            LoadLevel(1);
+        }
+
+        LoadLevel(nextLevel);
     }
 
     public void LevelFailed()
@@ -31,7 +68,7 @@ public class GameController : MonoBehaviour
         }
         else
         {
-
+            LoadLevel(level);
         }
     }
 }
